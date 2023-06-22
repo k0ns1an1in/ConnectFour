d0fb37812e108362a5c81bab083d41d49ce4bbf8
k0ns1an1in <konstantin.goebel@web.de>
2023-06-22 13:59:19 +0200

    clean sheet

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
-using namespace std;
-void jetztmaleinefunktion()
-{
-    cout << "Hallo Welt" << endl;
-}
-
 int main()
 {
     return 0;
-    // jetzt spiele ich hier ein bischen rum
-    // wir schreiben jetzt beide kommentare hier rum
-    jetztmaleinefunktion();
 }
