73f0cc3c4cf82caa7059a929a4dc760c9452835a
k0ns1an1in <konstantin.goebel@web.de>
2023-06-22 14:14:13 +0200

    setup Verwaltung header

--- Spielfeld.cpp
+++
@@ -1,1 +0,0 @@
-#include "Spielfeld.h"
--- Verwaltung.h
+++ Verwaltung.h
@@ -4,6 +4,9 @@ class Verwaltung
 public:
     Verwaltung();
     ~Verwaltung();
-
-
+    int* spielfeldErstellen();
+private:
+    int* spielfeld;
+    //todo Spieler1
+    //todo Spieler2
 };
