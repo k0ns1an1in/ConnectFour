20ac5c502f840ac88ab52997af0e57c7d5076abf
k0ns1an1in <konstantin.goebel@web.de>
2023-06-23 13:38:19 +0200

    unified matrix system

--- Verwaltung.cpp
+++ Verwaltung.cpp
@@ -5,18 +5,18 @@
 #include "allesmitspieler/ZufallsBot.hpp"
 #include "allesmitspieler/SchlauerBot.hpp"
 
-Verwaltung::Verwaltung(int spielerCode1, int spielerCode2):m_gui(GUI(&m_spielfeld[0][0]))
+Verwaltung::Verwaltung(int spielerCode1, int spielerCode2):m_gui(GUI(&m_spielfeld[0]))
 {
     switch(spielerCode1)
     {
         case 1:
         {
-            m_spieler1 = new VertikalerBot(&m_spielfeld[0][0]);
+            m_spieler1 = new VertikalerBot(&m_spielfeld[0]);
             break;
         }
         case 2:
         {
-            m_spieler1 = new HorizontalerBot(&m_spielfeld[0][0]);
+            m_spieler1 = new HorizontalerBot(&m_spielfeld[0]);
             break;
         }
         case 3:
@@ -26,12 +26,12 @@ Verwaltung::Verwaltung(int spielerCode1, int spielerCode2):m_gui(GUI(&m_spielfel
         }
         case 4:
         {
-            m_spieler1 = new ZufallsBot(&m_spielfeld[0][0]);
+            m_spieler1 = new ZufallsBot(&m_spielfeld[0]);
             break;
         }
         case 5:
         {
-            m_spieler1 = new SchlauerBot(&m_spielfeld[0][0]);
+            m_spieler1 = new SchlauerBot(&m_spielfeld[0], 1);
             break;
         }
     }
@@ -40,12 +40,12 @@ Verwaltung::Verwaltung(int spielerCode1, int spielerCode2):m_gui(GUI(&m_spielfel
     {
         case 1:
         {
-            m_spieler2 = new VertikalerBot(&m_spielfeld[0][0]);
+            m_spieler2 = new VertikalerBot(&m_spielfeld[0]);
             break;
         }
         case 2:
         {
-            m_spieler2 = new HorizontalerBot(&m_spielfeld[0][0]);
+            m_spieler2 = new HorizontalerBot(&m_spielfeld[0]);
             break;
         }
         case 3:
@@ -55,12 +55,12 @@ Verwaltung::Verwaltung(int spielerCode1, int spielerCode2):m_gui(GUI(&m_spielfel
         }
         case 4:
         {
-            m_spieler2 = new ZufallsBot(&m_spielfeld[0][0]);
+            m_spieler2 = new ZufallsBot(&m_spielfeld[0]);
             break;
         }
         case 5:
         {
-            m_spieler2 = new SchlauerBot(&m_spielfeld[0][0]);
+            m_spieler2 = new SchlauerBot(&m_spielfeld[0], 2);
             break;
         }
     }
@@ -73,11 +73,11 @@ GUI Verwaltung::getGui()
 
 int Verwaltung::spielsteinEinfuegen(int spalte, int spielerNummer)
 {
-    for(int i=5;i>=0;i--)
+    for(int i=0;i<6;i++)
     {
-        if(m_spielfeld[i][spalte] == 0)
+        if(m_spielfeld[spalte+i*7] == 0)
         {
-            m_spielfeld[i][spalte] = spielerNummer;
+            m_spielfeld[spalte+i*7] = spielerNummer;
             return 1;
         }
     }
@@ -92,7 +92,7 @@ int Verwaltung::siegUeberpruefen()
     {
         for(int x=0;x<7;x++)
         {
-            if(m_spielfeld[x][y] == 1)
+            if(m_spielfeld[x+y*7] == 1)
             {
                 spieler1counter++;
                 spieler2counter=0;
@@ -101,7 +101,7 @@ int Verwaltung::siegUeberpruefen()
                     return 1;
                 }
             }
-            if(m_spielfeld[x][y] == 2)
+            if(m_spielfeld[x+y*7] == 2)
             {
                 spieler2counter++;
                 spieler1counter=0;
@@ -110,7 +110,7 @@ int Verwaltung::siegUeberpruefen()
                     return 2;
                 }
             }
-            if(m_spielfeld[x][y] == 0)
+            if(m_spielfeld[x+y*7] == 0)
             {
                 spieler1counter=0;
                 spieler2counter=0;
@@ -124,7 +124,7 @@ int Verwaltung::siegUeberpruefen()
     {
         for(int y=0;y<6;y++)
         {
-            if(m_spielfeld[x][y]==1)
+            if(m_spielfeld[x+y*7]==1)
             {
                 spieler1counter++;
                 spieler2counter=0;
@@ -133,7 +133,7 @@ int Verwaltung::siegUeberpruefen()
                     return 1;
                 }
             }
-            if(m_spielfeld[x][y]==2)
+            if(m_spielfeld[x+y*7]==2)
             {
                 spieler2counter++;
                 spieler1counter=0;
@@ -142,7 +142,7 @@ int Verwaltung::siegUeberpruefen()
                     return 2;
                 }
             }
-            if(m_spielfeld[x][y]==0)
+            if(m_spielfeld[x+y*7]==0)
             {
                 spieler1counter=0;
                 spieler2counter=0;
