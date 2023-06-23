f42275a2763785334906e9e5154959bc4fc8196f
Suface <martinschweizer03@gmail.com>
2023-06-23 12:47:19 +0200

    vertikaler bot standart value, statt -1 auf 0 gesetzt

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Verwaltung.h"
 int mainpfusch()
 {
-    Verwaltung device = Verwaltung();
-    device.getGui().spielfeldDrucken();
+    // Verwaltung device = Verwaltung();
+    // device.getGui().spielfeldDrucken();
     return 0;
 }
