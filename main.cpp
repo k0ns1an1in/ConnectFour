a28cb2b735ed5006268370c9bbbd13ef1488f376
k0ns1an1in <konstantin.goebel@web.de>
2023-06-25 12:42:37 +0200

    cleaned main and added function spielen()

--- main.cpp
+++ main.cpp
@@ -5,20 +5,6 @@ int main()
 {
     int oscillator=0;
     Verwaltung device = Verwaltung(3, 3);
-    device.getGui().spielfeldDrucken();
-    while(device.siegUeberpruefen()==0)
-    {
-        if(!oscillator)
-        {
-            device.spielsteinEinfuegen(device.getSpieler1()->zug_zeile(), 1);
-            oscillator=!oscillator;
-        }else if(oscillator)
-        {
-            device.spielsteinEinfuegen(device.getSpieler2()->zug_zeile(), 2);
-            oscillator=!oscillator;
-        }
-        device.getGui().spielfeldDrucken();
-    }
-    cout << "Spieler " << !oscillator +1 << " hat gewonnen!" << endl;
+    device.spielen();
     return 0;
 }
